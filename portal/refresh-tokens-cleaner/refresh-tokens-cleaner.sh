409dd080ca53565228fee407e3d0388698573471
tmible <panevnikita@inbox.ru>
2025-05-07 09:24:42 +0300

    исправлена ошибка удаления refresh токенов

--- portal/refresh-tokens-cleaner/refresh-tokens-cleaner.sh
+++ portal/refresh-tokens-cleaner/refresh-tokens-cleaner.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sqlite3 $WISHLIST_DB_PATH <<< "DELETE FROM refresh_tokens WHERE expires < $(date +%s)"
+sqlite3 $WISHLIST_DB_FILE_PATH <<< "DELETE FROM refresh_tokens WHERE expires < $(date +%s)"
